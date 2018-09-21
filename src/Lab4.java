50cc2ccd8d29a9707e0c1ab5ad61cd3d6eeeacaa
Gurwinder Singh <gurwindersingh@Gurwinders-MBP.fios-router.home>
2018-09-20 21:34:05 -0400

    Lab4.java completetion

--- src/Lab4.java
+++ src/Lab4.java
@@ -2,27 +2,25 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class Lab4 {
+    public static Integer[] sortArray(Integer[] element){
+        Integer[] copyArray = new Integer[5];
+        System.arraycopy(element,0,copyArray,0,element.length);
+        Arrays.sort(copyArray, Collections.reverseOrder());
+        return copyArray;
 
-    public void main(String[] args) {
-        // write your code here
-        int[] array1 = new int[]{1, 2, 3, 4, 5};
-        int[] array2 = new int[]{10, 9, 8, 7, 6};
-        int[] array3 = new int[]{15, 11, 19, 7, 9};
-        int[] array4 = new int[]{100, 6, 16, 20, 3};
-        int[] array5 = new int[]{51, 3, 30, 21, 1};
-        try {
-            sortArray(array1);
-            sortArray(array2);
-            sortArray(array3);
-            sortArray(array4);
-            sortArray(array5);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Exception occurred");
-        }
     }
+    public static void main(String[] args){
+        Integer[] array1 = new Integer[]{1,2,3,4,5};
+        Integer[] array2 = new Integer[]{10,9,8,7,6};
+        Integer[] array3 = new Integer[]{15,11,19,7,9};
+        Integer[] array4 = new Integer[]{100,6,16,20,3};
+        Integer[] array5 = new Integer[]{51,3,30,21,1};
 
-    public static Integer[] sortArray(int[] srcArray) {
+        System.out.println(Arrays.toString(sortArray(array1)));
+        System.out.println(Arrays.toString(sortArray(array2)));
+        System.out.println(Arrays.toString(sortArray(array3)));
+        System.out.println(Arrays.toString(sortArray(array4)));
+        System.out.println(Arrays.toString(sortArray(array5)));
 
-        }
     }
 }
